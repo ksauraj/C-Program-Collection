8d2ac2a10b6decf766ba44097deaac3b3590e58d
ksauraj <gitsauraj@gmail.com>
2023-12-14 19:09:20 +0530

    matrix: add function for converting matrix to float type

--- matrix/matrix.c
+++ matrix/matrix.c
@@ -5,13 +5,19 @@
 int main(){
   int mat_a[4][3];
   matrix_creator_2d(4,3, (int*)&mat_a);
-  printf("\nCreated matrix is : \n"); // Checking Created matrix if its same as the user entered.
-  matrix_printer_2d(4,3, (int*)&mat_a);
+  printf("\nCreated matrix is : \n"); // Checking Created matrix if its same as the user entered...
+  matrix_printer_2d(4,3, (int*)&mat_a); // This funtion prints matric having int values...
 
   // Now lets create a transpose of matrix A.
   int mat_b[3][4];
   transpose_of_matrix(4, 3, (int*)&mat_a, (int*)&mat_b); // transpose_of_matrix('no of rows in org matrix', 'no of cols in org matrix', address of org matrix, address of transpose matrix)
   printf("\nTranspose of given matrix is : \n");
-  matrix_printer_2d(3,4, (int*)&mat_b);
+  matrix_printer_2d(3,4, (int*)&mat_b); // Note : Following function will fail if data type is other than int...
+
+  // Now converting mat B to float type and storing it in mat C.
+  float mat_c[3][4]; // Let's create a matrix with float data type...
+  covert_matrix_to_float(3, 4, (int*)&mat_b, (float*)&mat_c); // storing values of matrix B in matrix C 
+  printf("\nMatrix C : \n");
+  float_matrix_printer_2d(3, 4, (float*)&mat_c); // This function prints matrix having float values...
   return 0;
 }
--- matrix/matrix.h
+++ matrix/matrix.h
@@ -19,14 +19,13 @@ static void matrix_creator_2d(int n_rows, int n_col, int *matrix){
     }
   }
   //Print values of created matrix
-  printf("Matrix created with following Values : \n");
+  printf("\nMatrix created with following Values : \n");
   for(int i=0; i < n_rows; i++){
     for(int j=0; j < n_col; j++){
       printf("%d ", matrix_a[i][j]);
     }
     printf("\n");
   }
-  //return 0;
 }
 
 static void matrix_printer_2d(int n_rows, int n_cols, int *matrix){
@@ -40,9 +39,20 @@ static void matrix_printer_2d(int n_rows, int n_cols, int *matrix){
 
 }
 
+static void float_matrix_printer_2d(int n_rows, int n_cols, float *matrix){
+  for(int i=0; i < n_rows; i++){
+    for(int j=0; j < n_cols; j++){
+      printf("%.2f ", *matrix);
+      matrix++;
+    }
+    printf("\n");
+  }
+
+}
+
 static void transpose_of_matrix(int n_rows_org_mat, int n_cols_org_mat, int *matrix, int *t_matrix){
+  // This function finds transpose of given matrix a and stores in matrix b.
   int *reference_mat_add, *reference_t_mat_add;
-  //reference_mat_add = matrix;
   reference_t_mat_add = t_matrix;
   for(int i=0; i<n_rows_org_mat; i++){
     for(int j=0; j<n_cols_org_mat; j++){
@@ -53,6 +63,19 @@ static void transpose_of_matrix(int n_rows_org_mat, int n_cols_org_mat, int *mat
     reference_t_mat_add++;  
     t_matrix=reference_t_mat_add;
   }
+}
+
+
+static void covert_matrix_to_float(int n_rows_org_mat, int n_cols_org_mat, int *matrix, float *f_matrix){
+  // This function converts values of given matrix A from integer type to floating point values and stores in given matrix B.
+  float *reference_f_mat_add = f_matrix;
+  for (int i = 0; i<(n_rows_org_mat*n_cols_org_mat); i++){
+    *f_matrix = (float)*matrix;
+    f_matrix++;
+    matrix++;
+  }
+  printf("\nGiven matrix stored with these floating point values : \n"); 
+  float_matrix_printer_2d(n_rows_org_mat, n_cols_org_mat, reference_f_mat_add);
 }
 
 
